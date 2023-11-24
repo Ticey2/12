a4d23c10b6d833c2ce77ba6e20bc6f1ecc8e865d
ponOlgap <ponOlgap@gmail.com>
2023-11-24 10:16:56 +0700

    commin https

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Path, Query, Body, Header, status, Depends
+from fastapi.responses import HTMLResponse, PlainTextResponse, JSONResponse, FileResponse
+from fastapi.security import OAuth2PasswordBearer
+import uvicorn
+
+from public.users import users_router, info_router
+from public.goods import good_router
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+@app.get("/items/", response_class=JSONResponse)
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return JSONResponse(content={"token": token})
 
+app.include_router(users_router)
+app.include_router(info_router)
+app.include_router(good_router)
+@app.get('/header', response_class=JSONResponse)
+def root(user_agent: str = Header(), host: str = Header(), sec_ch_ua_platform: str = Header()):
+    return JSONResponse(content={"user_agent": user_agent, "host": host,"sec_ch_ua_platform": sec_ch_ua_platform})
+@app.get('/find', status_code=405)
+def notfind():
+    return {"message": "Нет такого ресурса"}
 @app.get('/')
-async def f_index():
-    return {"message": "World___", "keyroot": "Привет!"}
+def f_indexH():
 
+    content = {"FIO": "Половикова Ольга Николаевна"}
+    headers = {"X-Cat-Dog": "alone in the world", "Content-Language": "en-US"}
+    return JSONResponse(content=content, headers=headers)
 
-@app.get('/tools')
-async def f_indexT():
-    return "Good day!"
+# if __name__ == "__main__":
+#     cert_file = "/path/to/cert.pem"
+#     key_file = "/path/to/key.pem"
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, ssl-keyfile=key_file, ssl-certfile=cert_file)
---
+++ public/users.py
@@ -0,0 +1,111 @@
+import uuid
+from fastapi import APIRouter, Body, status, HTTPException
+from fastapi.responses import JSONResponse, FileResponse, Response
+from models.good import Main_User, Main_UserDB, New_Respons, Tags, Good
+import hashlib
+from typing import Union, Annotated
+from fastapi.encoders import jsonable_encoder
+
+# реализация маршрутов для операций c конкретными тегами - конкретизация роутера
+users_router = APIRouter(tags=[Tags.users])
+info_router = APIRouter(tags=[Tags.info])
+def coder_passwd(cod: str):
+    result = cod*2
+
+
+# Наша примитивна база данных
+users_list = [Main_UserDB(name='Ivanov', id=108, password= '**********'), Main_UserDB(name="Petrov", id=134, password="**********")]
+user_dict = {}
+
+
+# для поиска пользователя в списке
+def find_user(id: int) -> Union[Main_UserDB, None]:
+    for user in users_list:
+        if user.id == id:
+            return user
+    return None
+
+@users_router.get("/api/users", response_model=Union[list[Main_User], None], tags=[Tags.users], summary="Операция для")
+def get_users():
+    '''
+    Класс APIRouter принадлежит пакету FastAPI и создает операции пути для
+    нескольких маршрутов. Класс APIRouter предназначен для модульности и организации
+    маршрутизации.
+    Класс APIRouter работает так же, как и класс FastAPI. Однако uvicorn не может
+    использовать экземпляр APIRouter для обслуживания приложения, в отличие
+    от FastAPIs.
+    Маршруты, определенные с помощью класса APIRouter, должны быть добавлены в
+    к объекту fastapi для обеспечения их видимости.
+    '''
+    from fastapi.encoders import jsonable_encoder
+    return users_list
+
+
+@users_router.get("/api/users/{id}", response_model=Union[New_Respons, Main_User], tags=[Tags.users])
+def get_user(id: int, response: Response):
+    '''
+    получаем пользователя по id
+    '''
+    try:
+      user = find_user(id)
+
+      return user
+    except HTTPException:
+      return New_Respons(message = "Пользователь не найден")
+@info_router.get("/api/users/{id}-{id1}", response_model=Union[New_Respons, Main_User], tags=[Tags.info])
+def get_user_(id: int, id1: int, response: Response):
+    '''
+    получаем пользователя по id
+    '''
+    user = find_user(id)
+    print(user)
+    # если не найден, отправляем сообщение об ошибке
+    if user == None:
+        response.status_code = 404
+        return New_Respons(message="Пользователь не найден")
+    # если пользователь найден, отправляем его
+    else:
+        return user
+@users_router.get("/api/users_dict", response_model=Union[dict, New_Respons], tags=[Tags.users])
+def get_user_dict():
+    '''
+    получаем все записи словаря-базы
+    '''
+    return user_dict
+@users_router.post("/api/users", response_model=Union[Main_User, New_Respons], tags=[Tags.users])
+def create_user(item: Annotated[Main_User, Body(embed=True, description="Новый пользователь")]):
+
+    user = Main_UserDB(name=item.name, id=item.id, password=coder_passwd(item.name))
+
+    # добавляем объект в список
+    users_list.append(user)
+
+    # добавляем объект в словарь
+    user_encoder = jsonable_encoder(user)
+    user_dict[str(item.id)] = user_encoder
+    return user_encoder
+
+@users_router.put("/api/users", response_model=Union[Main_User, New_Respons], tags=[Tags.users])
+def edit_person(item: Annotated[Main_User, Body(embed=True, description="Изменяем данные для пользователя по id")]):
+    # получаем пользователя по id
+    user = find_user(item.id)
+    # если не найден
+    if user == None:
+        return New_Respons(message= "Пользователь не найден")
+    # если пользователь найден, изменяем его данные и отправляем обратно клиенту
+    #user.id = item.id
+    user.name = item.name
+    return user
+
+@users_router.delete("/api/users/{id}", response_model=Union[list[Main_User], None], tags=[Tags.users])
+def delete_person(id: int):
+    # получаем пользователя по id
+    user = find_user(id)
+
+    # если не найден
+    if user == None:
+        return New_Respons(message= "Пользователь не найден")
+
+    # если пользователь найден, удаляем его
+    users_list.remove(user)
+    return users_list
