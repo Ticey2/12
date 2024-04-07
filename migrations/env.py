a11a380ba95aefc6dcae43b3ebb839e07e9c0b5a
ponOlgap <ponOlgap@gmail.com>
2024-04-07 17:26:19 +0700

    befor_deploy4

--- migrations/env.py
+++ migrations/env.py
@@ -19,6 +19,7 @@ config.set_section_option(section, 'POSTGRES_PASSWORD', settings.POSTGRES_PASSWO
 config.set_section_option(section, 'POSTGRES_DB', settings.POSTGRES_DB)
 config.set_section_option(section, 'POSTGRES_HOST', settings.POSTGRES_HOST)
 
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
